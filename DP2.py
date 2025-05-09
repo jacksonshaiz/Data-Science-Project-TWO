2db0ca49fee06752eb70c38f84715bb4f2159e6c
jacksonshaiz <uye3kg@virginia.edu>
2025-05-09 02:27:48 -0400

    Update DP2.py

--- DP2.py
+++ DP2.py
@@ -8,8 +8,8 @@ import pandas as pd
 app = Flask(__name__)
 
 # My spotify credentials from the Developer Dashboard// Spotify API
-SPOTIPY_CLIENT_ID = '9a99af1c909c44dda2cf1413b254bf90'
-SPOTIPY_CLIENT_SECRET = '278da29281654aaab559911cf61b42c7'
+SPOTIPY_CLIENT_ID = ''
+SPOTIPY_CLIENT_SECRET = ''
 
 # Using Spotify Credentials to authorize/log in
 auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
